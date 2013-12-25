3c4e55de46815cf6f132cbb7184d91a4a2892811
Joshy Cyriac <j.cyriac@gmail.com>
2013-12-25 23:42:42 +0100

    added addStrings and made code simpler

--- lib/src/trie.dart
+++ lib/src/trie.dart
@@ -2,13 +2,21 @@ library trie;
 
 class Trie {
   TrieNode root = new TrieNode.root();
-  
-  void addString(String text) {
-    root.addString(text);
+
+  void addStrings(List<String> words) {
+    if (words == null || words.isEmpty) return;
+    for (String word in words) {
+      root.addString(word);
+    }
   }
-  
-  bool contains(String text) {
-    return root.contains(text);
+
+  void addString(String word) {
+    if (word == null || word.isEmpty) return;
+    root.addString(word);
+  }
+
+  bool contains(String word) {
+    return root.contains(word);
   }
 }
 
@@ -17,12 +25,12 @@ class TrieNode {
   bool isWord = false;
   TrieNode parent = null;
   List<TrieNode> childs = [];
-  
-  
+
+
   TrieNode.root();
-  
+
   TrieNode(this.currentChar, this.parent);
-  
+
   void addString(String text) {
     if (text.isEmpty) {
       this.isWord = true;
@@ -30,43 +38,37 @@ class TrieNode {
     }
     var firstChar = text.substring(0, 1);
     var restString = text.substring(1);
-    
+
     if (childs.isEmpty) {
       var newChild = new TrieNode(firstChar, this);
       childs.add(newChild);
       newChild.addString(restString);
-    } else {    
+    } else {
       TrieNode newNode = childs.firstWhere(
-        (e) => e.currentChar == firstChar, 
+        (e) => e.currentChar == firstChar,
         orElse: () {
           TrieNode newNode = new TrieNode(firstChar, this);
           childs.add(newNode);
           return newNode;
         });
       newNode.addString(restString);
-    }    
-  }
-  
-  bool contains(String word) {
-    if (word.isEmpty) return false;
-    var firstChar = word.substring(0, 1);
-    
-    if (childs.isNotEmpty) {
-     var childNode = childs.firstWhere(
-         (e) => e.currentChar == firstChar, orElse: () => null);
-     
-     if (childNode == null) {
-       return false;
-     } else {
-       var restString = word.substring(1);
-       return restString == '' 
-           ? childNode.isWord : childNode.contains(restString);
-     }
-    } else {
-      return false;
     }
   }
-  
+
+  bool contains(String word) {
+    if (word.isEmpty || childs.isEmpty) return false;
+
+    var firstChar = word.substring(0, 1);
+    var childNode = childs.firstWhere(
+        (e) => e.currentChar == firstChar,
+        orElse: () => null);
+    if (childNode == null) return false;
+
+    var restString = word.substring(1);
+    return restString == '' ?
+        childNode.isWord : childNode.contains(restString);
+  }
+
   toString() => "($currentChar, $childs)";
 }
 
--- test/trie_test.dart
+++ test/trie_test.dart
@@ -2,61 +2,79 @@ import 'package:trie/src/trie.dart';
 import 'package:unittest/unittest.dart';
 
 void main() {
-  
+
   Trie trie = new Trie();
   trie.addString('one');
   trie.addString('two');
   trie.addString('car');
   trie.addString('cat');
   trie.addString('a');
-  
+
   // positive checks
-  
-  test('insert a should contain it', () {    
-    expect(trie.contains('a'), true); 
-  });
-  
-  test('insert one should contain it', () {    
-    expect(trie.contains('one'), true); 
-  });
-  
-  test('insert two should contain it', () {    
-    expect(trie.contains('two'), true); 
-  });
-  
-  test('insert car should contain it', () {    
-    expect(trie.contains('car'), true); 
-  });
-  
-  trie.contains('cat');
-  test('insert cat should contain it', () {    
-    expect(trie.contains('cat'), true); 
-  });
-  
-  test('insert two should contain two', () {    
-    expect(trie.contains('two'), true); 
+
+  test('insert a should contain it', () {
+    expect(trie.contains('a'), true);
   });
 
-  test('insert t should contain it', () {    
-    expect(trie.contains('a'), true); 
+  test('insert one should contain it', () {
+    expect(trie.contains('one'), true);
   });
-  
+
+  test('insert two should contain it', () {
+    expect(trie.contains('two'), true);
+  });
+
+  test('insert car should contain it', () {
+    expect(trie.contains('car'), true);
+  });
+
+  trie.contains('cat');
+  test('insert cat should contain it', () {
+    expect(trie.contains('cat'), true);
+  });
+
+  test('insert two should contain two', () {
+    expect(trie.contains('two'), true);
+  });
+
+  test('insert t should contain it', () {
+    expect(trie.contains('a'), true);
+  });
+
   // negative checks
-  
+
   test('trie should NOT contain on', () {
-    expect(trie.contains('on'), false); 
+    expect(trie.contains('on'), false);
   });
-  
-  test('trie should NOT contain tw', () {    
-    expect(trie.contains('tw'), false); 
+
+  test('trie should NOT contain tw', () {
+    expect(trie.contains('tw'), false);
   });
-  
+
   test('trie should NOT contain o', () {
     expect(trie.contains('o'), false);
-  }); 
-  
+  });
+
   test('trie should NOT contain \'\' ', () {
     expect(trie.contains(''), false);
-  }); 
+  });
+
+  // testing addStrings
+  test('trie should contain one and two ', () {
+    trie = new Trie();
+    trie.addStrings(['one', 'two']);
+    expect(trie.contains('one'), true);
+    expect(trie.contains('two'), true);
+  });
+
+  Trie trie1 = new Trie();
+  trie1.addStrings(['one', 'one']);
+  // testing adding duplicates
+  test('trie should contain one ', () {
+
+    expect(trie1.contains('one'), true);
+    expect(trie1.contains('two'), false);
+  });
+
 
 }
